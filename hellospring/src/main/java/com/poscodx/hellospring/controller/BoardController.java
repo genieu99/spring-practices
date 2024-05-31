4da97c13a6c4e14038c1305da509277a209e816e
genieu99 <yujin0419@sookmyung.ac.kr>
2024-05-31 13:03:37 +0900

    class: @RequestMapping #1

---
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.poscodx.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/board/write")
+	public String write() {
+		return "BoardController.write()";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/board/view")
+	public String view() {
+		return "BoardController.view()";
+	}
+}
--- hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -1,7 +1,15 @@
 package com.poscodx.hellospring.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -9,5 +17,49 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {
+		System.out.println("name: " + name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		
+		return mav;
+	}
+	
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+//		model.addAttribute("name1", name);
+//		ModelAndView mav = new ModelAndView();
+//		mav.addObject("name2", name);
+//		return "/WEB-INF/views/hello3.jsp";
+		
+		model.addAttribute("name1", name);
+		return "/WEB-INF/views/hello3.jsp";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/hello5")
+	public String hello5() {
+		return "<h1>Hello World</h1>";
+	}
+	
+	@RequestMapping("/hello6")
+	public String hello6() {
+		return "redirect:/hello";
+	}
+	
+	public void hello7(
+			HttpServletRequest request,
+			HttpServletResponse response
+			) throws IOException {
+		response.getWriter().print("<h1>Hello World</h1>");
 	}
 }
